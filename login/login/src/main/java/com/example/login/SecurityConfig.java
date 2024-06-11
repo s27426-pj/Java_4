250251c3900ba97729a5d6254d245e51ede473c8
MateuszFalkowski01 <s27426@pjwstk.edu.pl>
2024-06-11 14:19:42 +0200

    kod_z_lekcji

---
+++ login/login/src/main/java/com/example/login/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.login;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Getter
+@Setter
+@Configuration
+@EnableWebSecurity
+@ConfigurationProperties("security.config")
+public class SecurityConfig {
+
+    private String frontendRedirectUrl;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/login").permitAll();
+                    request.anyRequest().authenticated();
+                })
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .oauth2Login(l -> l.defaultSuccessUrl(frontendRedirectUrl, true))
+                .build();
+    }
+}
