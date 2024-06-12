ba11fc962e7f78586a3df5af803e39e41d214e95
s27426-pj <s27426@pjwstk.edu.pl>
2024-06-12 15:48:57 +0200

    patch_v-1.1

--- Zjazd6/Zjazd6/zad1/src/main/java/zj1/example/zad1/login/WebConfig.java
+++ Zjazd6/Zjazd6/zad1/src/main/java/zj1/example/zad1/login/WebConfig.java
@@ -9,11 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -24,19 +19,6 @@ public class WebConfig {
 
     private String frontendRedirectUrl;
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
